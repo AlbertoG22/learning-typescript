50514176f84ea169109dd25b9b9afff66f524ed2
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-11-24 15:13:00 -0600

    readonly modifier

--- 02-classes-interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 02-classes-interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,10 +1,10 @@
 class Department {
-    // private id: string;
+    // private readonly id: string;
     // private name: string;
     // private property
     private employees: string[] = [];
 
-    constructor( private id: string, public name: string) {
+    constructor( private readonly id: string, public name: string) {
         // this.id = id;
         // this.name = n;
     }
