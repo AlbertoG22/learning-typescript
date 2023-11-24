6b96cfc7979d1774282d1420067c8461bc88ae3a
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-11-24 14:38:32 -0600

    private and public access modifiers

--- 02-classes-interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 02-classes-interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,5 +1,7 @@
 class Department {
     name: string;
+    // private property
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
@@ -8,10 +10,25 @@ class Department {
     describe(this:Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
-accounting.describe();
+accounting.addEmployee('Sergio');
+accounting.addEmployee('Alberto');
 
-const accountingCopy = { describe: accounting.describe };
-accountingCopy.describe(); // WRONG! -- not "name" property on accountingCopy obj
+accounting.addEmployee[2] = 'Martha'; // esto ya no es posible al hacer private la propiedad
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { describe: accounting.describe };
+// accountingCopy.describe(); // WRONG! -- no hay propiedad "name" en el objeto accountingCopy
