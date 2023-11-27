7b461f76a34485c98e111dd92a5b74b2c2379908
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-11-27 13:39:13 -0600

    type guards (typeof, in, instanceof)

--- 03-advance-types/src/app.ts
+++ 03-advance-types/src/app.ts
@@ -22,3 +22,54 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  //Type Guard
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+  }
+}
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log('driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if(vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+useVehicle(v1);
+useVehicle(v2);
