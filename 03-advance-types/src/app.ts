070e40252e1a87013be2a8869cd3d0fcfa405de7
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-11-27 14:47:05 -0600

    type casting

--- 03-advance-types/src/app.ts
+++ 03-advance-types/src/app.ts
@@ -100,3 +100,9 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+
+const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
+
+userInputElement.value = 'Hi there!';
