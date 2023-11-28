59e824672aad68c4ee419f0a51989d377b5aa939
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-11-28 14:33:51 -0600

    keyof constraint

--- 04-generics/src/app.ts
+++ 04-generics/src/app.ts
@@ -38,3 +38,9 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
     return [element, descriptionText];
 }
 console.log(countAndDescribe('Hi there!'));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return 'Value: ' + obj[key];
+}
+// extractAndConvert({}, 'name'); // no es posible porque no hay property name en el objeto
+extractAndConvert({ name: 'Beto' }, 'name');
