dce7258339e2e6fa21067ab90fc16b36bd83462b
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-11-28 20:42:34 -0600

    property decorators

--- 05-decorators/src/app.ts
+++ 05-decorators/src/app.ts
@@ -1,4 +1,5 @@
 function Logger(logString: string) {
+    console.log('LOGGER FACTORY');
     return function(constructor: Function) {
         console.log(logString);
         console.log(constructor);
@@ -6,6 +7,7 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY');
     return function(constructor: any) {
         const hookEl = document.getElementById(hookId);
         const p = new constructor(); // usar los valores de la clase dentro de la decorator function
@@ -16,7 +18,7 @@ function WithTemplate(template: string, hookId: string) {
     }
 }
 
-// @Logger('LOGGIN - PERSON')
+@Logger('LOGGIN - PERSON')
 @WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = 'Beto';
@@ -28,3 +30,30 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
+// ------------------------------------------------------------------------------------------------
+function Log(target: any, propertyName: string | symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    set price(val: number) {
+        if(val > 0) {
+            this._price = val;
+        } else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
