1991bc45fa99750f43d8e82fadb6198bca75e6db
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-12-04 14:49:34 -0600

    adding a listener to a form with private method

--- 06-drag&drop-project/src/app.ts
+++ 06-drag&drop-project/src/app.ts
@@ -18,7 +18,23 @@ class ProjectInput {
         this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
 
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        /* 
+            el "this" en esta función no apunta al "this" de la clase, esto ya que
+            al lanzar un evento, el "this" va a referir al objetivo del evento, 
+            es decir, al form: <form id="user-input">…</form>
+        */
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        // por eso usamos el bind()
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
