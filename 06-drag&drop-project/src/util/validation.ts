fbe5bd1b71ac1807f0f57b706866f92914553aa9
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-12-07 20:25:50 -0600

    organizing files and folders 1

---
+++ 06-drag&drop-project/src/util/validation.ts
@@ -0,0 +1,32 @@
+namespace App {
+    // Validation
+    export interface Validatable {
+        value: string | number;
+        require?: boolean;
+        minLength?: number;
+        maxLength?: number;
+        min?: number;
+        max?: number;
+    }
+    
+    export function validate(validatableInput: Validatable) {
+        let isValid = true;
+        if(validatableInput.require) {
+            isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+        }
+        if(validatableInput.minLength != null && typeof validatableInput.value === 'string') { // "!=" para incluir casos en que sea null o undefined
+            isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+        }
+        if(validatableInput.maxLength != null && typeof validatableInput.value === 'string') { // "!=" para incluir casos en que sea null o undefined
+            isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+        }
+        if(validatableInput.min != null && typeof validatableInput.value === 'number') {
+            isValid = isValid && validatableInput.value >= validatableInput.min;
+        }
+        if(validatableInput.max != null && typeof validatableInput.value === 'number') {
+            isValid = isValid && validatableInput.value <= validatableInput.max;
+        }
+    
+        return isValid;
+    }
+}
