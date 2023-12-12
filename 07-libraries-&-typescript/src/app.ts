af4cdfcaa3d08175b2537e74f5ffed6b6363489e
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-12-11 20:17:31 -0600

    using js libraries with ts

---
+++ 07-libraries-&-typescript/src/app.ts
@@ -0,0 +1,5 @@
+/* se instaló un 'translation type package' (@types/lodash), el cual sirve como traducción de 
+librerías de JS, para poder usarlas en TS */
+import _ from 'lodash'; 
+
+console.log(_.shuffle([1, 2, 3]));
