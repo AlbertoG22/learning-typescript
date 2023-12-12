5df2dad987000d09b4aa2c4ad947047af5ff3f02
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-12-11 21:05:06 -0600

    ts embracing (class-validator)

--- 07-libraries-&-typescript/src/app.ts
+++ 07-libraries-&-typescript/src/app.ts
@@ -1,8 +1,8 @@
 /* se instaló un 'translation type package' (@types/lodash), el cual sirve como traducción de 
 librerías de JS, para poder usarlas en TS */
 import _ from 'lodash';
-
 import 'reflect-metadata';
+
 import { plainToClass } from 'class-transformer';
 
 /* 'declare' le permite a TS declarar features o variables que sabemos que existen, 
@@ -17,11 +17,23 @@ console.log(GLOBAL);
 
 // --------------------------------------------------------------------------------------------
 import { Product } from './product.model';
+import { validate } from 'class-validator';
+
 
 const products = [
     { title: 'A carpet', price: 29.99 },
     { title: 'A book', price: 10.99 }
 ];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+    if(errors.length > 0) {
+        console.log('VALIDATION ERRORS!');
+        console.log(errors);
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
 
 // const loadedProducts = products.map(prod => {
 //     return new Product(prod.title, prod.price);
