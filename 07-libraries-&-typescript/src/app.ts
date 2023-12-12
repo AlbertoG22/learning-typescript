078e28c2a1d6a615126d3d82d4aa251f849e886d
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-12-11 20:40:54 -0600

    using 'declare'

--- 07-libraries-&-typescript/src/app.ts
+++ 07-libraries-&-typescript/src/app.ts
@@ -2,4 +2,11 @@
 librerías de JS, para poder usarlas en TS */
 import _ from 'lodash'; 
 
+/* 'declare' le permite a TS declarar features o variables que sabemos que existen, 
+o hacerle saber a TS sobre variables globales que algunos paquetes incluyen, 
+mismas que TS no puede saber que existen, pero nosotros como developers sí */
+declare var GLOBAL: string;
+
 console.log(_.shuffle([1, 2, 3]));
+
+console.log(GLOBAL);
